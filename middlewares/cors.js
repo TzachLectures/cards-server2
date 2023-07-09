8e04ef4037cb6d59387e3336e2b5a74413d7a167
Benny Dabush <bennydabush@gmail.com>
2023-07-09 19:56:17 +0300

    cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,11 @@
 const cors = require("cors");
 
 const corsOptions = {
-  origin: ["http://127.0.0.1:5500", "http://localhost:3000"],
+  origin: [
+    "http://127.0.0.1:5500",
+    "http://localhost:3000",
+    "https://master--effortless-gingersnap-b51f26.netlify.app",
+  ],
   optionsSuccessStatus: 200,
 };
 
